627796b7824d82712a3250c288c842047264a258
Luiz Oliveira <valuiz1@gmail.com>
2022-03-25 12:26:01 +0000

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn booleangenerator.wsgi --logfile -
---
+++ script_site/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScriptSiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'script_site'
