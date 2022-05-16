43898a0fc6d20a7e92f0c952c5e3942f32c6cf36
LuizGOCunha <valuiz1@gmail.com>
2022-05-16 14:53:46 -0300

    Boolean Generator logo

--- booleangenerator/settings.py
+++ booleangenerator/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
